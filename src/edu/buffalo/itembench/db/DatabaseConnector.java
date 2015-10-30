e595d1856d2645feef45814fa12af11d514e07eb
pketki <ketki.purandare23@gmail.com>
2015-10-30 14:49:42 -0400

    DatabaseConnector basic code

--- src/edu/buffalo/itembench/db/DatabaseConnector.java
+++ src/edu/buffalo/itembench/db/DatabaseConnector.java
@@ -3,32 +3,68 @@
  */
 package edu.buffalo.itembench.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
- * @author jlimaye
+ * @author pketki
  * 
  */
 public class DatabaseConnector {
 
-	private String name;
+	private String driver;
+	private String connectionURL;
 	private String user;
 	private String password;
+	private String dbName;
+	private Connection connection;
 
-	/**
-	 * @param name
-	 * @param user
-	 * @param password
-	 */
-	public DatabaseConnector(String name, String user, String password) {
+	public DatabaseConnector(Properties props) {
 		super();
-		this.name = name;
-		this.user = user;
-		this.password = password;
+		driver = props.getProperty("driver");
+		connectionURL = props.getProperty("url");
+		user = props.getProperty("user");
+		password = props.getProperty("password");
+		dbName = props.getProperty("dbName");
 	}
 
-	public Object getConnection(){return null;}
+	public Connection getConnection() throws DbException {
+		try {
+			Class.forName(driver);
+			connection = DriverManager.getConnection(connectionURL + dbName,
+					user, password);
+		} catch (ClassNotFoundException e) {
+			throw new DbException("Incorrect Database Driver");
+		} catch (SQLException e) {
+			throw new DbException("Unable to connect to DB");
+		}
 
-	public boolean query(Object query){return false;}
+		return connection;
+	}
 
-	public void closeConnection(){}
+	public ResultSet query(String query) throws DbException {
+		ResultSet resultSet = null;
+		try {
+			// TODO: Use statement instead?
+			PreparedStatement statement = connection.prepareStatement(query);
+			resultSet = statement.executeQuery();
+		} catch (SQLException e) {
+			throw new DbException("Invalid query or unable to connect to DB");
+		}
+		return resultSet;
+	}
+
+	public void closeConnection() throws DbException {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			throw new DbException("Unable to close connection");
+		}
+		connection = null;
+	}
 
 }
---
+++ src/edu/buffalo/itembench/db/DbException.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package edu.buffalo.itembench.db;
+
+/**
+ * @author pketki
+ *
+ */
+public class DbException extends Exception {
+
+	/**
+	 * 
+	 */
+	public DbException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public DbException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 */
+	public DbException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public DbException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 * @param arg2
+	 * @param arg3
+	 */
+	public DbException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
