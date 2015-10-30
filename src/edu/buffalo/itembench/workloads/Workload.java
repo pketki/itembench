499ce2d0d6d713aff6cb07a40b3c2d239791cc9e
pketki <ketki.purandare23@gmail.com>
2015-10-30 15:40:21 -0400

    Added init method to Workloads for initializations like creating tables

--- src/edu/buffalo/itembench/workloads/Workload.java
+++ src/edu/buffalo/itembench/workloads/Workload.java
@@ -23,10 +23,36 @@ public abstract class Workload {
 	 */
 	public Workload(int readLoad, int writeLoad, int updateLoad) {
 		this();
+		this.setReadLoad(readLoad);
+		this.setWriteLoad(writeLoad);
+		this.setUpdateLoad(updateLoad);
+	}
+
+	public int getReadLoad() {
+		return readLoad;
+	}
+
+	public void setReadLoad(int readLoad) {
 		this.readLoad = readLoad;
+	}
+
+	public int getWriteLoad() {
+		return writeLoad;
+	}
+
+	public void setWriteLoad(int writeLoad) {
 		this.writeLoad = writeLoad;
+	}
+
+	public int getUpdateLoad() {
+		return updateLoad;
+	}
+
+	public void setUpdateLoad(int updateLoad) {
 		this.updateLoad = updateLoad;
 	}
+
+	public abstract void init();
 
 	public abstract void run();
 }
