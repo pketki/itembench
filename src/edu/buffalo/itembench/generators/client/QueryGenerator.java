48ba12833145dfddbf04cda71750711b75995c08
Jitendra Limaye <jlimaye@buffalo.edu>
2015-12-10 18:49:55 -0500

    Code cleanup

---
+++ src/edu/buffalo/itembench/generators/client/QueryGenerator.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.buffalo.itembench.generators.client;
+
+import java.sql.PreparedStatement;
+
+/**
+ * @author Jitendra
+ *
+ */
+public class QueryGenerator {
+
+	public PreparedStatement getLoadQuery(){
+		return null;
+	}
+}
